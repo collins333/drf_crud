a9dea96e0c0d8c8b72ae42bbbc075adfec672b29
collins7 <collins.mdfp@gmail.com>
2024-01-26 16:17:38 +0100

    first commit

---
+++ proyectos/api.py
@@ -0,0 +1,9 @@
+from .models import Proyecto
+from rest_framework import viewsets, permissions
+from .serializers import ProyectoSerializer
+
+class ProyectoViewset(viewsets.ModelViewSet):
+  queryset = Proyecto.objects.all()
+  permission_classes = [permissions.AllowAny]
+  serializer_class = ProyectoSerializer
+ 
